905bcd81abce272a756abeec42cf8cf92c4ce2f0
Hugh Dickinson <hugh.dickinson@open.ac.uk>
2021-03-08 19:27:09 +0000

    Added package explicitly

--- setup.py
+++ setup.py
@@ -3,4 +3,4 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(packages=["FacilityLocation"], install_requires=["numba", "numpy"])
+setuptools.setup(packages=["."], install_requires=["numba", "numpy"])
